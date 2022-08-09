523db8e347da8786f9d09bf4b0fabff3546e2674
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-09 12:01:52 -0400

    added manifest format from init input

--- tools/manifestInitalizer.cs
+++ tools/manifestInitalizer.cs
@@ -14,7 +14,7 @@ namespace JLDN.tools
             Console.Write(Prompt);
             dynamic input = Console.ReadLine();
             bool res = false;
-            if (String.IsNullOrWhiteSpace(input)) { Console.WriteLine("Project name must not be NULL."); res = false; Environment.Exit(0); }
+            if (String.IsNullOrWhiteSpace(input)) { Console.WriteLine("\nField must not be NULL."); res = false; Environment.Exit(0); }
             else
             {
                 res = true;
@@ -24,12 +24,11 @@ namespace JLDN.tools
         public static void initalizeManifest()
         {
             string Dir = Directory.GetCurrentDirectory();
-            string prjName = manifestInitalizer.getManifestInfoInput("Project Name: ");
             string pkgName = manifestInitalizer.getManifestInfoInput("Package Name: ");
             string pkgDescription = manifestInitalizer.getManifestInfoInput("Package Description: ");
             string libDirectory = manifestInitalizer.getManifestInfoInput("Path to library directory: " + Directory.GetCurrentDirectory().ToString() + "\\");
-
-
+            string manifestFormat = String.Format("version: 0.0.1\npackage_name: {0}\npackage_description: {1}\nlibrary_directory: \\{2}", pkgName, pkgDescription, libDirectory);
+            Console.WriteLine(manifestFormat);
         }
     }
 }
