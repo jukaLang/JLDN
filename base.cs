7c18bdb254bc742841c793292abbbced2ad8b73a
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-08 23:46:22 -0400

    Update base.cs

--- base.cs
+++ base.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace JLDN
+{
+    public class CLI
+    {
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("JLDN Version 0.0.001");
+            }
+            if (args.Length >= 0 && args[1]=="help")
+            {
+                Console.WriteLine("Help Command Called");
+            }
+        }
+    }
+}
+
