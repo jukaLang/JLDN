97d3e8658220933dabce9d5d99cf76e33d3fd576
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-09 22:00:53 -0400

    attempted api request for files

--- tools/repoData.cs
+++ tools/repoData.cs
@@ -4,18 +4,68 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JLDN.tools
 {
+    public class TREE_DATA
+    {
+        public string sha { get; set; }
+        public string url { get; set; }
+        public bool truncated { get; set; }
+    }
+    public class BLOB_DATA
+    {
+        public string path { get; set; }
+        public string mode { get; set; }
+        public string type { get; set; }
+        public string sha { get; set; }
+        public string url { get; set; }
+    }
     internal class repoData
     {
-        public static string returnRepoFiles(string repoAuthor, string repoName, string repoBranch)
+        public static void toJson(dynamic contents)
         {
-            string url = "https://api.github.com/repos/jukalang/jldn/git/trees/main?recursive=1";
-            //string url = String.Format("https://api.github.com/repos/{0}/{1}/git/trees/{2}?recursive=1", repoAuthor, repoName, repoBranch);
-            Console.WriteLine(url);
-            string data = JLDN.network.webResParser.fetchWebResAsync(url);
+
+            foreach (var file in contents)
+            {
+                var fileType = (string)file["type"];
+                if (fileType == "dir")
+                {
+                    var directoryContentsUrl = (string)file["url"];
+                    // use this URL to list the contents of the folder
+                    Console.WriteLine($"DIR: {directoryContentsUrl}");
+                }
+                else if (fileType == "file")
+                {
+                    var downloadUrl = (string)file["download_url"];
+                    // use this URL to download the contents of the file
+                    Console.WriteLine($"DOWNLOAD: {downloadUrl}");
+                }
+            }
+        }
+        public static void blobList(dynamic data)
+        {
+            foreach(var file in data)
+            {
+                var fileType = (string)file["type"];
+                if (fileType == "blob")
+                {
+                    var blobContentsUrl = (string)file["url"];
+                    Console.WriteLine($"DIR: {blobContentsUrl}");
+                }
+            }
+        }
+        public static dynamic returnRepoFiles(string repoAuthor, string repoName, string repoBranch)
+        {
+            var repo = String.Format("{0}/{1}", repoAuthor, repoName);
+            string url = String.Format("https://api.github.com/repos/{0}/contents", repo);
+            dynamic data = JLDN.network.webResParser.fetchWebResAsync(url);
             Console.WriteLine("Web server: Fetching lib contents");
+
+            dynamic jsonData = toJson(data);
+            blobList(jsonData);
             return data;
 
         }
